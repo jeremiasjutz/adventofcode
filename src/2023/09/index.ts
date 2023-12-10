4c146834f7af56c2c6cb0f009f5ac0d88195272c
Jeremias Jutz <jutz.jeremias@gmail.com>
2023-12-10 13:46:02 +0100

    day9 fail

--- src/2023/09/index.ts
+++ src/2023/09/index.ts
@@ -1,7 +1,22 @@
+import { sum } from "../../utils";
+
 // Setup
-const input = Bun.file("input");
+const input = Bun.file("exampleinput");
 const lines = (await input.text()).split("\n");
 
 // Part 1
+let result = 0;
+for (const line of lines) {
+  let parts = line.split(" ").map(Number);
+  const prevLastNum = [parts.at(-1) ?? 0];
+  while (parts.some((n) => n !== 0)) {
+    parts = parts
+      .map((n, i, original) => Math.abs(n - (original?.[i + 1] ?? 0)))
+      .slice(0, -1);
+    prevLastNum.push(parts.at(-1) ?? 0);
+  }
+  result += prevLastNum.reduce(sum, 0);
+}
+console.log(result);
 
 // Part 2
